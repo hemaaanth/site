cb7c44f9df43ee6c1638798a43677aaee5b4ec41
Hemanth Soni <git@soni.im>
2024-11-29 16:22:52 +0100

    add filtering capabilities and update places

--- components/Places/index.tsx
+++ components/Places/index.tsx
@@ -3,17 +3,16 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { FaUtensils, FaGlassMartini, FaLandmark, FaStore, FaBed, FaCoffee, FaBriefcase, FaBeer } from 'react-icons/fa';
 
-type PlaceType = 'food' | 'drink' | 'activity' | 'sight' | 'store' | 'hotel' | 'coffee' | 'work' | 'party';
+export type PlaceType = 'food' | 'drink' | 'activity' | 'sight' | 'store' | 'hotel' | 'coffee' | 'work' | 'party' | 'landmark';
 
 interface PlaceItemProps {
     title: string;
     location: string;
     googleMapsUrl: string;
-    type: PlaceType;
+    types: PlaceType[];
     description: string;
     image?: string;
-    icon: string;
-  }
+}
 
 const typeIcons = {
   food: FaUtensils,
@@ -28,34 +27,37 @@ const typeIcons = {
 };
 
 const PlaceItem: React.FC<PlaceItemProps> = ({
-title,
-location,
-googleMapsUrl,
-icon,
-description,
-image,
-}) => {
-    const Icon = typeIcons[icon] || FaLandmark;
-
-return (
-    <div className="mb-8">
-    <div className="flex items-center mb-2">
-        <Icon className="mr-2 text-neutral-500" />
-        <h3 className="font-semibold">{title}</h3>
-    </div>
-    <div className="flex items-center text-sm text-neutral-500 mb-2">
-        <a href={googleMapsUrl} target="_blank" className="hover:underline">
-        {location}
-        </a>
-    </div>
-    {image && (
-        <div className="mb-4">
-        <Image src={image} alt={title} width={500} height={300} className="rounded-md" />
+    title,
+    location,
+    googleMapsUrl,
+    types = [], // Add default empty array
+    description,
+    image,
+  }) => {
+    return (
+      <div className="mb-8">
+        <div className="flex items-center mb-2">
+          <div className="flex gap-2 mr-2">
+            {types?.map((type) => { // Add optional chaining
+              const Icon = typeIcons[type] || FaLandmark;
+              return <Icon key={type} className="text-neutral-500" />;
+            })}
+          </div>
+          <h3 className="font-semibold">{title}</h3>
         </div>
-    )}
-    <p className="mb-4">{description}</p>
-    </div>
-);
-};
+        <div className="flex items-center text-sm text-neutral-500 mb-2">
+          <a href={googleMapsUrl} target="_blank" className="hover:underline truncate">
+            {location}
+          </a>
+        </div>
+        {image && (
+          <div className="mb-4">
+            <Image src={image} alt={title} width={500} height={300} className="rounded-md" />
+          </div>
+        )}
+        <p className="mb-4">{description}</p>
+      </div>
+    );
+  };
 
 export default PlaceItem;
